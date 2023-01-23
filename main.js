a4fdf7ff9eea1e85f04713aa6754dade6cfa66c6
MarthaSharkeyKing <mking@scottlogic.com>
2023-01-23 11:47:43 +0000

    add basic message

---
+++ main.js
@@ -0,0 +1,8 @@
+toggleHidden = () => {
+    var x = document.getElementById("role");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
